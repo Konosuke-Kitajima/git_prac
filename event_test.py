d2ef5d593ffc99dd74649cd726a95759838dc740
Konosuke-Kitajima <114898320+Konosuke-Kitajima@users.noreply.github.com>
2025-02-01 13:34:42 +0000

    リセット

--- event_test.py
+++ event_test.py
@@ -1,6 +1,6 @@
 import qrcode
 print("qrcodeを作成します。")
-input_data = input("QRコードにいれるデータを入力してください!!!：")
+input_data = input("QRコードにいれるデータを入力してください：")
 
 qr = qrcode.QRCode(
     
@@ -9,6 +9,6 @@ qr.add_data(input_data)
 qr.make(fit=True)
 
 img = qr.make_image(fill_color="black", back_color="white").convert('L')
-save_path = input("保存する名前を決めてください!!!!!!：　")
+save_path = input("保存する名前を決めてください：　")
 img.save(save_path + '.png')
 img.show()
