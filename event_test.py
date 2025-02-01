80fd839b33e7ddd42f564eda6b788e3e5545e572
kitajima <kkitaji88641@gmail.com>
2025-02-01 15:10:40 +0900

    ファイルを追加
    for git teet

---
+++ event_test.py
@@ -0,0 +1,14 @@
+import qrcode
+print("qrcodeを作成します。")
+input_data = input("QRコードにいれるデータを入力してください：")
+
+qr = qrcode.QRCode(
+    
+)
+qr.add_data(input_data)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white").convert('L')
+save_path = input("保存する名前を決めてください：　")
+img.save(save_path + '.png')
+img.show()
